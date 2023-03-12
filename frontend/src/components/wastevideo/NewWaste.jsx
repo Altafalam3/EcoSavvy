5c7f6c5e4b4ea62fa35e860acc52f5e344981642
Saarthak Kumar <110285246+saar18hak@users.noreply.github.com>
2023-03-12 08:53:59 +0530

    last commit

--- frontend/src/components/wastevideo/NewWaste.jsx
+++ frontend/src/components/wastevideo/NewWaste.jsx
@@ -1,27 +1,51 @@
-import React from "react";
-
-const NewWaste = (props) => {
-  const data = props.data;
+import React from 'react'
+import P1 from './p1.png'
+import P2 from './p2.png'
+import P3 from './p3.png'
+const NewWaste = () => {
   return (
     <div>
-      <h3 className="text-center text-2xl text-bold mt-10">Results</h3>
-      <div className="mt-4">
-        {data.map((result, index) => {
-          const wasteType = result.label;
-          const confidence = parseFloat(result.confidence.toFixed(2));
-          if (confidence === "plastic") {
-            return (
-              <p key={index}>
-                Waste type: {wasteType}, Confidence: {confidence}
-                HEllo bacho
-              </p>
-            );
-          }
-          return null;
-        })}
+      <section className="text-gray-400 bg-gray-900 body-font">
+      <div className="container px-5 py-24 mx-auto">
+        <div className="flex flex-col">
+          <div className="h-1 bg-gray-800 rounded overflow-hidden">
+            <div className="w-24 h-full bg-green-500"></div>
+          </div>
+          <div className="flex flex-wrap sm:flex-row flex-col py-6 mb-12">
+            <h1 className="sm:w-2/5 text-white font-medium title-font text-2xl mb-2 sm:mb-0">Space The Final Frontier</h1>
+            <p className="sm:w-3/5 leading-relaxed text-base sm:pl-10 pl-0">Street art subway tile salvia four dollar toast bitters selfies quinoa yuccie synth meditation iPhone intelligentsia prism tofu. Viral gochujang bitters dreamcatcher.</p>
+          </div>
+        </div>
+        <div className="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4">
+          <div className="p-4 md:w-1/3 sm:mb-0 mb-6">
+            <div className="rounded-lg h-64 overflow-hidden">
+              <img alt="content" className="object-cover object-center h-full w-full" src={P2} />
+            </div>
+            <h2 className="text-xl font-medium title-font text-white mt-5">Shooting Stars</h2>
+            <p className="text-base leading-relaxed mt-2">Swag shoindxgoitch literally meditation subway tile tumblr cold-pressed. Gastropub street art beard dreamcatcher neutra, ethical XOXO lumbersexual.</p>
+           
+          </div>
+          <div className="p-4 md:w-1/3 sm:mb-0 mb-6">
+            <div className="rounded-lg h-64 overflow-hidden">
+              <img alt="content" className="object-cover object-center h-full w-full" src={P3} />
+            </div>
+            <h2 className="text-xl font-medium title-font text-white mt-5">The Catalyzer</h2>
+            <p className="text-base leading-relaxed mt-2">Swag shoindxigoitch literally meditation subway tile tumblr cold-pressed. Gastropub street art beard dreamcatcher neutra, ethical XOXO lumbersexual.</p>
+            
+          </div>
+          <div className="p-4 md:w-1/3 sm:mb-0 mb-6">
+        <div className="rounded-lg h-64 overflow-hidden">
+          <img alt="content" className="object-cover object-center h-full w-full" src={P1}/>
+        </div>
+        <h2 className="text-xl font-medium title-font text-white mt-5">The 400 Blows</h2>
+        <p className="text-base leading-relaxed mt-2">Swag shoindegoitch literally meditation subway tile tumblr cold-pressed. Gastropub street art beard dreamcatcher neutra, ethical XOXO lumbersexual.</p>
+        
       </div>
     </div>
-  );
-};
+  </div>
+</section>  
+    </div>
+  )
+}
 
-export default NewWaste;
+export default NewWaste
